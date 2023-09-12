02dd634f74669f8d1b7ef5d5d76b400e3cf0c92a
aaparky <aa.parky@gmail.com>
2023-09-12 15:15:50 +0100

    some error checking logic, to be removed

--- thankful/thankful.lua
+++ thankful/thankful.lua
@@ -1,22 +1,31 @@
 local f = CreateFrame("Frame")
 local lastBuffApplier
 
--- event fires when a unit's aura changes
+-- This event is fired when a unit's aura (buff or debuff) changes.
 f:RegisterEvent("UNIT_AURA")
 
-f:SetScript("OnEvent", function (self, event, arg1)
+f:SetScript("OnEvent", function(self, event, arg1)
     if event == "UNIT_AURA" and arg1 == "player" then
-        for i = 1, 40 do
+        print("UNIT_AURA event detected!")  -- Debugging print
+        
+        for i = 1, 40 do -- In Classic, a player can have a max of 32 buffs but using 40 for a safer margin.
             local name, _, _, _, _, _, _, caster = UnitBuff("player", i)
-            if name and caster then
+            
+            if name then
+                print("Detected buff:", name)  -- Debugging print
+            end
+            
+            if caster then
                 local playerName = UnitName(caster)
-
+                print("Buff caster:", playerName)  -- Debugging print
+                
+                -- Check if the buff is from another player (and not an NPC or the player themselves).
                 if playerName and playerName ~= UnitName("player") and playerName ~= lastBuffApplier then
                     lastBuffApplier = playerName
-                    SendChatMessage("Thanks for the buff, " .. playerName .."!", "SAY")
+                    SendChatMessage("Thanks for the buff, " .. playerName .. "!", "SAY")
+                    break -- Exit the loop once we've thanked the player.
                 end
             end
         end
     end
-    
 end)
