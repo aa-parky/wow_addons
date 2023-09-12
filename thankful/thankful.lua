36e268c44b3e915e53374ca0cec496915fe887c7
aaparky <aa.parky@gmail.com>
2023-09-12 15:23:08 +0100

    more logic debugging changes

--- thankful/thankful.lua
+++ thankful/thankful.lua
@@ -1,29 +1,31 @@
 local f = CreateFrame("Frame")
 local lastBuffApplier
 
--- This event is fired when a unit's aura (buff or debuff) changes.
 f:RegisterEvent("UNIT_AURA")
 
 f:SetScript("OnEvent", function(self, event, arg1)
     if event == "UNIT_AURA" and arg1 == "player" then
-        print("UNIT_AURA event detected!")  -- Debugging print
+        print("UNIT_AURA event detected!")
         
-        for i = 1, 40 do -- In Classic, a player can have a max of 32 buffs but using 40 for a safer margin.
+        for i = 1, 40 do
             local name, _, _, _, _, _, _, caster = UnitBuff("player", i)
             
             if name then
-                print("Detected buff:", name)  -- Debugging print
+                print("Detected buff:", name, "from caster:", caster)  -- Updated debug print
             end
             
-            if caster then
-                local playerName = UnitName(caster)
-                print("Buff caster:", playerName)  -- Debugging print
+            local playerName = UnitName(caster)
+            print("PlayerName from UnitName:", playerName)  -- Debugging print
+            
+            if playerName then
+                print("Condition 1 (is playerName):", playerName ~= nil)
+                print("Condition 2 (is not the player):", playerName ~= UnitName("player"))
+                print("Condition 3 (is not the lastBuffApplier):", playerName ~= lastBuffApplier)
                 
-                -- Check if the buff is from another player (and not an NPC or the player themselves).
                 if playerName and playerName ~= UnitName("player") and playerName ~= lastBuffApplier then
                     lastBuffApplier = playerName
                     SendChatMessage("Thanks for the buff, " .. playerName .. "!", "SAY")
-                    break -- Exit the loop once we've thanked the player.
+                    break
                 end
             end
         end
