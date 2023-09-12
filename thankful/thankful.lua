01a96481c543f5ba54fe70d092763bc12bb4b95e
aaparky <aa.parky@gmail.com>
2023-09-12 16:33:35 +0100

    debug

--- thankful/thankful.lua
+++ thankful/thankful.lua
@@ -1,57 +1,81 @@
-local f = CreateFrame("Frame")
-
--- Register for buff/debuff changes on the player
-f:RegisterEvent("UNIT_AURA")
-
-f:SetScript("OnEvent", function(self, event, arg1)
-    if event == "UNIT_AURA" and arg1 == "player" then
-        for i = 1, 40 do
-            local name, _, _, _, _, _, _, _, caster = UnitBuff("player", i)
-            if name and caster and caster ~= "player" then
-                local casterName = UnitName(caster)
-                if casterName then
-                    local playerLink = "|Hplayer:"..casterName.."|h["..casterName.."]|h"
-                    print(playerLink .. " cast " .. name .. " on you.")
-                end
-            end
-        end
-    end
-end)
-
 local Timers = {
-    DisplayDuration = 10,
     SaveTime = 2 * 60000,
-    DefaultDuration = 10, 
     DefaultSaveTime = 2 * 60000,
 }
 
 local PlayerGUID, LastTime, Ticker
-local BuffText = "%s Buffed you with: %s" -- %s 1 = player link, %s 2 = buff name
+local BuffText = "%s %s Buffed you with: %s"
 local SavedBuffs = {}
 
 local function RemoveOldBuffs()
     if #SavedBuffs == 0 then return end
     local time = debugprofilestop()
-    for i=#SavedBuffs, 1, -1 do
+    for i = #SavedBuffs, 1, -1 do
         if LastTime and SavedBuffs[i].time + Timers.SaveTime < time then
             tremove(SavedBuffs, i)
         end
     end
-    if #SavedBuffs == 0 then
+    if #SavedBuffs == 0 and Ticker then
         Ticker:Cancel()
         Ticker = nil
     end
 end
 
--- ... rest of the original code ...
-
-if bit.band(sourceFlags, COMBATLOG_OBJECT_TYPE_PLAYER) == COMBATLOG_OBJECT_TYPE_PLAYER and (subevent == "SPELL_AURA_APPLIED" or subevent == "SPELL_AURA_REFRESH") and auraType == "BUFF" and destGUID == PlayerGUID and sourceGUID ~= PlayerGUID then
-    LastTime = debugprofilestop()
+local f = CreateFrame("Frame")
+f:RegisterEvent("PLAYER_LOGIN")
+f:RegisterEvent("PLAYER_ENTERING_WORLD")
+f:RegisterEvent("PLAYER_LEAVING_WORLD")
+f:SetScript("OnEvent", function(self, event, ...)
+    if event == "PLAYER_LOGIN" then
+        PlayerGUID = UnitGUID("player")
+        return
+    end
+    if event == "PLAYER_ENTERING_WORLD" then
+        print("Entering World!")
+        C_Timer.After(2, function()
+            self:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+            print("Registered COMBAT_LOG_EVENT_UNFILTERED!")
+        end)
+        return
+    end
+    if event == "PLAYER_LEAVING_WORLD" then
+        self:UnregisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+        return
+    end
+    if event == "COMBAT_LOG_EVENT_UNFILTERED" then
+        print("Combat log event detected!")
+    end
     
-    -- ... rest of the event logic ...
-
-    local playerLink = "|Hplayer:"..sourceName.."|h["..sourceName.."]|h"
-    local text = format(BuffText, playerLink, spellName)
+    local timestamp, subevent, _, sourceGUID, sourceName, sourceFlags, _, destGUID, _, _, _, spellID, spellName, _, auraType = CombatLogGetCurrentEventInfo()
+    sourceName = sourceName or COMBATLOG_UNKNOWN_UNIT
     
-    -- ... rest of the event logic ...
+    if bit.band(sourceFlags, COMBATLOG_OBJECT_TYPE_PLAYER) == COMBATLOG_OBJECT_TYPE_PLAYER and (subevent == "SPELL_AURA_APPLIED" or subevent == "SPELL_AURA_REFRESH") and auraType == "BUFF" and destGUID == PlayerGUID and sourceGUID ~= PlayerGUID then
+        LastTime = debugprofilestop()
+        local _, class, _, _, _, _, realm = GetPlayerInfoByGUID(sourceGUID)
+        
+        if realm == "" then
+            realm = GetRealmName()
+        end
+        
+        sourceName = "|c" .. select(4, GetClassColor(class)) .. sourceName
+        realm = "|cffffff00[" .. realm .. "]|r"
+        local text = format(BuffText, sourceName, realm, spellName)
+        print("Attempting to print message:", text)
+        
+        tinsert(SavedBuffs, { time = LastTime, spell = spellName, msg = text })
+        print(text)
+        
+        if not Ticker then
+            Ticker = C_Timer.NewTicker(5, RemoveOldBuffs)
+        end
+    end
+end)
+
+local function SortByTime(a, b)
+    return a.time > b.time
+end
+
+SLASH_SWARFBUFF1 = "/swb"
+SlashCmdList["SWARFBUFF"] = function(msg)
+    -- ... rest of the slash command logic ...
 end
