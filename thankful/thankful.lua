764929e10315989a88c7a2282f3ae2e5fd8c2723
aaparky <aa.parky@gmail.com>
2023-09-12 15:30:40 +0100

    testing id player who cast buff

--- thankful/thankful.lua
+++ thankful/thankful.lua
@@ -1,31 +1,16 @@
 local f = CreateFrame("Frame")
-local lastBuffApplier
 
+-- Register for buff/debuff changes on the player
 f:RegisterEvent("UNIT_AURA")
 
 f:SetScript("OnEvent", function(self, event, arg1)
     if event == "UNIT_AURA" and arg1 == "player" then
-        print("UNIT_AURA event detected!")
-        
         for i = 1, 40 do
             local name, _, _, _, _, _, _, caster = UnitBuff("player", i)
-            
-            if name then
-                print("Detected buff:", name, "from caster:", caster)  -- Updated debug print
-            end
-            
-            local playerName = UnitName(caster)
-            print("PlayerName from UnitName:", playerName)  -- Debugging print
-            
-            if playerName then
-                print("Condition 1 (is playerName):", playerName ~= nil)
-                print("Condition 2 (is not the player):", playerName ~= UnitName("player"))
-                print("Condition 3 (is not the lastBuffApplier):", playerName ~= lastBuffApplier)
-                
-                if playerName and playerName ~= UnitName("player") and playerName ~= lastBuffApplier then
-                    lastBuffApplier = playerName
-                    SendChatMessage("Thanks for the buff, " .. playerName .. "!", "SAY")
-                    break
+            if name and caster and caster ~= "player" then
+                local casterName = UnitName(caster)
+                if casterName then
+                    print(casterName .. " cast " .. name .. " on you.")
                 end
             end
         end
