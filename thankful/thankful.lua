f0bc37a38f98347dc80917e98b01590705769d6f
aaparky <aa.parky@gmail.com>
2023-09-14 12:20:55 +0100

    bug repair

--- thankful/thankful.lua
+++ thankful/thankful.lua
@@ -11,9 +11,14 @@ f:SetScript("OnEvent", function(self, event, ...)
         return
     end
     if event == "PLAYER_ENTERING_WORLD" then
-        C_Timer.After(2, function()
-            self:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
-        end)
+        local inInstance = IsInInstance()
+        if not inInstance and GetNumGroupMembers() == 0 then
+            C_Timer.After(2, function()
+                self:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+            end)
+        else
+            self:UnregisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+        end
         return
     end
     if event == "PLAYER_LEAVING_WORLD" then
@@ -23,11 +28,6 @@ f:SetScript("OnEvent", function(self, event, ...)
 
     local _, subevent, _, sourceGUID, sourceName, sourceFlags, _, destGUID, _, _, _, _, spellName, _, auraType = CombatLogGetCurrentEventInfo()
     sourceName = sourceName or COMBATLOG_UNKNOWN_UNIT
-
-    -- Direct check to ensure you're not in any group or instance.
-    if IsInGroup() or IsInInstance() then
-        return
-    end
 
     if bit.band(sourceFlags, COMBATLOG_OBJECT_TYPE_PLAYER) == COMBATLOG_OBJECT_TYPE_PLAYER and (subevent == "SPELL_AURA_APPLIED" or subevent == "SPELL_AURA_REFRESH") and auraType == "BUFF" and destGUID == PlayerGUID and sourceGUID ~= PlayerGUID then
         local _, class, _, _, _, _, realm = GetPlayerInfoByGUID(sourceGUID)
